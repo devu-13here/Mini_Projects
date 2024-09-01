d33890ebd37069ed2790eb13e24592d54add2fb9
Devanjali Patel <150452256+devu-13here@users.noreply.github.com>
2024-09-01 14:54:44 +0530

    Add files via upload
    This is the basic qr code generator. (*_*)

---
+++ Project1.py
@@ -0,0 +1,10 @@
+import qrcode as qr
+
+
+#Provide the link here okk!
+img = qr.make("https://www.kaggle.com/devanjalipatel")
+img.save("devanjali's_kaggle.png")
+
+
+#The above code will generate the qr simply.
+
